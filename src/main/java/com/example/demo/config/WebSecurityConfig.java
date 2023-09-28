897f945a352f243dda1cfe2d0bf9e9194df41b29
SIPDOMAIN\nyizeya <nyizeya@startinpoint.com>
2023-09-28 12:01:24 -0700

    feat(WebSecurityConfig.java): add cors filter bean

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.config;
 import com.example.demo.security.authentication.CustomAuthenticationProvider;
 import com.example.demo.security.filter.InitialJwtFilter;
 import com.example.demo.security.utility.LogoutService;
+import com.google.common.collect.Lists;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +29,20 @@ public class WebSecurityConfig {
     private final LogoutService logoutService;
 
     @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // Allow requests from these origins
+        config.addAllowedOrigin("http://localhost:4200"); // Add the origins you want to allow
+        config.setAllowedMethods(Lists.newArrayList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L); // 1 hour
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
@@ -34,6 +52,7 @@ public class WebSecurityConfig {
                 .anyRequest()
                 .authenticated();
 
+        http.addFilterBefore(corsFilter(), InitialJwtFilter.class);
         http.addFilterBefore(initialJwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
